8bcdf3d5e4fe2c15ec632727e5606cdceab13c5e
Tim Pratt <tim_pratt2003@yahoo.com>
2019-11-01 12:34:57 -0700

    io practice

---
+++ homeclasswork/ranfilenumwrite.py
@@ -0,0 +1,17 @@
+#writes random numbers to a file between 1-100.
+import random
+def rnfw():
+    #how many numbers to be written
+    totnum = int(input("How many random numbers? "))
+
+    #open file
+    f = open('rnfw.txt','w+')
+    #writes lines
+    linecount = 0
+    while linecount < totnum:
+        f.write(str(random.randrange(1, 100)))
+        f.write('\n')
+        linecount += 1
+    #close file
+    f.close()
+rnfw()
