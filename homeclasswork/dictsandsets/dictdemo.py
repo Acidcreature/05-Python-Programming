2f2fe1535e56b4b4f64341bfaea1b7e14dfc4c43
Tim Pratt <tim_pratt2003@yahoo.com>
2019-11-05 12:56:50 -0800

    renamed

--- homeclasswork/dictsandsets/dictdemo.py
+++ homeclasswork/dictsandsets/dictdemo.py
@@ -100,26 +100,25 @@ print(phonebook)
 key, value = phonebook.popitem()
 print(key, value)
 """
-# Multi Dimensional Dictionary
-​
-# Intitialize instrument dictionary
+# Multi Dimensional Dictionary​
+# Initialize instrument dictionary
 instruments = {'drums': {'color':'black', 'sound':'boom'},
-               'guitar': {'color':'blue', 'sound':'wahhh'}}
+                'guitar': {'color':'blue', 'sound':'wahhh'}}
 print('Original', instruments)
-​
+
 # Add something to the nested dictionary
 instruments.update({'violin':{'color':'brown', 'sound':'whineeee'}})
 print('After updating', instruments)
-​
-# More ways to add instruments
+
+#more ways to add instruments
 instruments['bass'] = {'color':'purple', 'sound':'slappadabass'}
 print('After adding bass', instruments)
-​
+
 # Access items/nested items within our dictionary
 print('guitar', instruments['guitar'])
 print('guitar sound', instruments['guitar']['sound'])
-​
-# Iterate through nested dictionary
+
+#Iterate through nested dictionary
 for instrument, properties in instruments.items():
     print('Instrument: ', instrument)
     for property in properties:
