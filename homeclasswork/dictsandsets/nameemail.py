36619acd4ef3823c942d1d6a94a1d24aed0de65f
Tim Pratt <tim_pratt2003@yahoo.com>
2019-11-07 08:34:19 -0800

    classwork

--- homeclasswork/dictsandsets/nameemail.py
+++ homeclasswork/dictsandsets/nameemail.py
@@ -18,11 +18,12 @@ def nameemail():
     try:
         #open file if this is not the first time the program runs
         nefileprint = open('nameemail.dat', 'rb')
-        nebin = pickle.load(nefileprint)
-        print(nebin)
+        nedata = pickle.load(nefileprint)
+        print(nedata)
     except:
         # Dictionary of stored names and emails
         nedata = {'Jon Snow' : 'winter1$coming@whitewalker.com', 'Bruce Wayne' : 'imB@tman@darkcave.com'}
+    finally:
         # Who is the user looking for?
         entry = nedata.get(input('Please enter the name of the person you\'d like to contact via email. '))
         # Print the requested info, or none.
@@ -36,20 +37,24 @@ def nameemail():
             1. Add a new Name/Email. \n \
             2. Change an existing email. \n \
             3. Delete a Name/Email. \n \
-            4. type \'exit\' to stop. \n ")
+            4. Type \'exit\' to stop. \n ")
             # First loop, add names/emails.
             while numchoice != '0' or numchoice != 'exit':
                 if numchoice == '1':
                     addname = input("Please enter the Name to add. ")
                     addemail = input("Please enter the Email to add. ")
                     nedata.update({addname : addemail})
+                    print('\n')
                     print(nedata)
+                    print('\n')
                     #input validation
-                    numchoice = input("Do you need to enter more data? Enter 1 to continue \
-                       \n 0 to go back to main menue, or \'exit\' to end. ")
+                    numchoice = input("Do you need to enter more data? Enter 1 to continue, \
+                       \n 0 to go back to main menue, or \'exit\' to end. \n ")
+                       
                     while numchoice != '0' and numchoice != '1' and numchoice != 'exit':
-                        numchoice = input("Do you need to enter more data? Enter 1 to continue \
-                       \n 0 to go back to main menue, or \'exit\' to end. ")
+                        numchoice = input("Do you need to enter more data? Enter 1 to continue, \
+                       \n 0 to go back to main menue, or \'exit\' to end. \n")
+                       
                 else:
                     break
                 #Second loop for editting emails
@@ -63,13 +68,15 @@ def nameemail():
                         # Changes are entered, added to dictionary, and print completion.
                         emailchange = input("Enter new email. ")
                         nedata.update({namechange : emailchange})
+                        print('\n')
                         print(nedata)
+                        print('\n')
                         #input validation
-                        numchoice = input("Do you need to enter more data? Enter 2 to continue \
-                       \n 0 to go back to main menue, or \'exit\' to end. ")
+                        numchoice = input("Do you need to enter more data? Enter 2 to continue, \
+                       \n 0 to go back to main menue, or \'exit\' to end. \n ")
                         while numchoice != '0' and numchoice != '2' and numchoice != 'exit':
-                            numchoice = input("Do you need to enter more data? Enter 2 to continue \
-                       \n 0 to go back to main menue, or \'exit\' to end. ")
+                            numchoice = input("Do you need to enter more data? Enter 2 to continue, \
+                       \n 0 to go back to main menue, or \'exit\' to end. \n ")
                 else:
                     break
                 #Third loop for deleting entries
@@ -79,12 +86,14 @@ def nameemail():
                     #Data to delete
                     userdel = input("Enter a user name to delete ")
                     del nedata[userdel]
+                    print('\n')
                     print(nedata)
+                    print('\n')
                     #input validation
-                    numchoice = input("Do you need to enter more data? Enter 3 to continue \
+                    numchoice = input("Do you need to enter more data? Enter 3 to continue, \
                        \n 0 to go back to main menue, or \'exit\' to end. ")
                     while numchoice != '0' and numchoice != '3' and numchoice != 'exit':
-                        numchoice = input("Do you need to enter more data? Enter 3 to continue \
+                        numchoice = input("Do you need to enter more data? Enter 3 to continue, \
                        \n 0 to go back to main menue, or \'exit\' to end. ")
                 else:
                     break
